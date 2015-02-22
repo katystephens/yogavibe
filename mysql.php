7d0bb5c23801fdd2da3368fb153e83c767dc8a48
Katy Stephens <katyrstephens@gmail.com>
2015-02-22 11:19:51 -0700

    fixed mysql for multiple support

--- mysql.php
+++ mysql.php
@@ -1,7 +1,18 @@
 <?php
 
-//create connection
-//$conn = mysqli_connect("mysql.cs.dixie.edu", "kstephens", "Katy123", "kstephens");
-$conn = mysqli_connect("localhost", "katy", "katy123", "yogavibe");
+	$server = $_SERVER['HTTP_HOST'];
+	//create connection
+	if($server === 'local.yogavibe.com')
+	{
+		$conn = mysqli_connect("yogavibe.org", "katy", "katy123", "yogavibe");
+	}
+	else if($server === 'kstephens.php.cs.dixie.edu')
+	{
+		$conn = mysqli_connect("mysql.cs.dixie.edu", "kstephens", "Katy123", "kstephens");
+	}
+	else
+	{
+		$conn = mysqli_connect("localhost", "katy", "katy123", "yogavibe");
+	}
 
 ?>
